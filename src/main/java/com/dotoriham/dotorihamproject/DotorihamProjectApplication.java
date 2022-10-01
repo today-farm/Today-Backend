502a99e669787ebc4d64cc9bc4b3d7b747cb2fd6
KSH_coding <95729738+KSH-beginner@users.noreply.github.com>
2022-10-01 22:58:17 +0900

    init

---
+++ src/main/java/com/dotoriham/dotorihamproject/DotorihamProjectApplication.java
@@ -0,0 +1,13 @@
+package com.dotoriham.dotorihamproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DotorihamProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DotorihamProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/dotoriham/dotorihamproject/DotorihamProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dotoriham.dotorihamproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DotorihamProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
