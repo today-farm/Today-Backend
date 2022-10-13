17645282a03c73d4a2ff276fab6adf6808420dd0
KSH_coding <95729738+KSH-beginner@users.noreply.github.com>
2022-10-13 23:46:32 +0900

    feat : UserRepository 추가

---
+++ src/main/java/com/today/todayproject/domain/user/repository/UserRepository.java
@@ -0,0 +1,13 @@
+package com.today.todayproject.domain.user.repository;
+
+import com.today.todayproject.domain.user.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+
+    Optional<User> findByEmail(String email);
+
+    Optional<User> findByRefreshToken(String refreshToken);
+}
