a4985f16ef06bde62b6b3500eae9f7162bb36cdc
KSH_coding <95729738+KSH-beginner@users.noreply.github.com>
2022-10-13 23:53:07 +0900

    공통 JSON Response 추가

---
+++ src/main/java/com/today/todayproject/global/BaseResponse.java
@@ -0,0 +1,29 @@
+package com.today.todayproject.global;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public class BaseResponse<T> {
+
+    private final Boolean isSuccess;
+    private final String message;
+    private final int code;
+    private T result;
+
+    // 요청 성공 시 (생성자 파라미터에 데이터 넘어옴)
+    public BaseResponse(T result) {
+        this.isSuccess = BaseResponseStatus.SUCCESS.isSuccess();
+        this.message = BaseResponseStatus.SUCCESS.getMessage();
+        this.code = BaseResponseStatus.SUCCESS.getCode();
+        this.result = result;
+    }
+
+    // 요청 실패 시 (생성자 파라미터에 BaseResponseStatus 예외 넘어옴)
+    public BaseResponse(BaseResponseStatus status) {
+        this.isSuccess = status.isSuccess();
+        this.message = status.getMessage();
+        this.code = status.getCode();
+    }
+}
---
+++ src/main/java/com/today/todayproject/global/BaseResponseStatus.java
@@ -0,0 +1,27 @@
+package com.today.todayproject.global;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+/**
+ * 에러 코드 관리
+ */
+@Getter
+@AllArgsConstructor
+public enum BaseResponseStatus {
+
+    /**
+     * 1000 : 요청 성공
+     */
+    SUCCESS(true, 1000, "요청에 성공하였습니다.");
+
+    /**
+     * 2000 : Request 오류
+     */
+
+
+    private final boolean isSuccess;
+    private final int code;
+    private final String message;
+
+}
