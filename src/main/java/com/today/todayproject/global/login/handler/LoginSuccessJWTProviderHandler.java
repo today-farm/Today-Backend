1fa0e5f437a16d315001a7b732978ed2f7470702
KSH_coding <95729738+KSH-beginner@users.noreply.github.com>
2022-10-13 23:51:01 +0900

    feat : LoginHandler 추가

---
+++ src/main/java/com/today/todayproject/global/login/handler/LoginSuccessJWTProviderHandler.java
@@ -0,0 +1,53 @@
+package com.today.todayproject.global.login.handler;
+
+import com.today.todayproject.domain.user.repository.UserRepository;
+import com.today.todayproject.global.jwt.service.JwtService;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.web.authentication.SimpleUrlAuthenticationSuccessHandler;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@Slf4j
+@RequiredArgsConstructor
+public class LoginSuccessJWTProviderHandler extends SimpleUrlAuthenticationSuccessHandler {
+
+    private final JwtService jwtService;
+    private final UserRepository userRepository;
+
+    @Value("${jwt.access.expiration}")
+    private String accessTokenExpiration;
+
+    @Override
+    public void onAuthenticationSuccess(HttpServletRequest request, HttpServletResponse response, Authentication authentication) throws IOException, ServletException {
+
+        String email = extractUsername(authentication); // 인증 정보에서 Username(email) 추출
+        String accessToken = jwtService.createAccessToken(email); // JwtServiceImpl의 createAccessToken을 사용하여 AccessToken 발급
+        String refreshToken = jwtService.createRefreshToken(); // JwtServiceImpl의 createRefreshToken을 사용하여 RefreshToken 발급
+
+        jwtService.sendAccessAndRefreshToken(response, accessToken, refreshToken); // 헤더에 accessToken, refreshToken을 실어서 요청
+
+        // DB에 refreshToken 저장
+        userRepository.findByEmail(email).ifPresent(
+                user -> user.updateRefreshToken(refreshToken)
+        );
+
+
+        log.info( "로그인에 성공합니다. eamil: {}" ,email);
+        log.info( "AccessToken 을 발급합니다. AccessToken: {}" ,accessToken);
+        log.info( "RefreshToken 을 발급합니다. RefreshToken: {}" ,refreshToken);
+        log.info("Access Token 만료 기간 : {}", accessTokenExpiration);
+    }
+
+
+    private String extractUsername(Authentication authentication){
+        UserDetails userDetails = (UserDetails) authentication.getPrincipal();
+        return userDetails.getUsername();
+    }
+}
