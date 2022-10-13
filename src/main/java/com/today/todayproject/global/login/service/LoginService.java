a7b918532516f0bb754df95d0cb63f8432d010f7
KSH_coding <95729738+KSH-beginner@users.noreply.github.com>
2022-10-13 23:49:58 +0900

    feat : UserDetailsService를 구현한 커스텀 클래스 추가

---
+++ src/main/java/com/today/todayproject/global/login/service/LoginService.java
@@ -0,0 +1,33 @@
+package com.today.todayproject.global.login.service;
+
+import com.today.todayproject.domain.user.User;
+import com.today.todayproject.domain.user.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+/**
+ * UserDetailsService를 구현한 커스텀 LoginService
+ * UserDetailsService의 loadUserByUsername을 Override하여 구현
+ * DB에서 해당 Email로 유저를 찾은 후, 해당 유저의 정보로 UserDetails 객체를 생성(Builder)하여 반환
+ */
+@Service
+@RequiredArgsConstructor
+public class LoginService implements UserDetailsService {
+
+    private final UserRepository userRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+        User user = userRepository.findByEmail(email)
+                .orElseThrow(() -> new UsernameNotFoundException("해당 이메일이 없습니다."));
+
+        return org.springframework.security.core.userdetails.User.builder()
+                .username(user.getEmail())
+                .password(user.getPassword())
+                .roles(user.getRole().name())
+                .build();
+    }
+}
