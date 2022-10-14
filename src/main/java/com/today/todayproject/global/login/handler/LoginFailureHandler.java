481317a649e94c2558cab3a0c9ea5100e17fa906
KSH_coding <95729738+KSH-beginner@users.noreply.github.com>
2022-10-14 12:18:26 +0900

    refactor : 로그인 실패 메세지 수정

--- src/main/java/com/today/todayproject/global/login/handler/LoginFailureHandler.java
+++ src/main/java/com/today/todayproject/global/login/handler/LoginFailureHandler.java
@@ -15,7 +15,7 @@ public class LoginFailureHandler extends SimpleUrlAuthenticationFailureHandler {
     @Override
     public void onAuthenticationFailure(HttpServletRequest request, HttpServletResponse response, AuthenticationException exception) throws IOException, ServletException {
         response.setStatus(HttpServletResponse.SC_BAD_REQUEST);
-        response.getWriter().write("check username or password");
+        response.getWriter().write("로그인 실패! 이메일이나 비밀번호를 확인해주세요.");
         log.info("로그인에 실패했습니다. 메시지 : {}", exception.getMessage());
     }
 }
