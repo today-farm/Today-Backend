b0e6ce2312cad5c76b97e8d38583dca24d3b0f7a
KSH_coding <95729738+KSH-beginner@users.noreply.github.com>
2022-10-14 13:52:53 +0900

    feat : 닉네임, 이메일 관련 예외 추가

--- src/main/java/com/today/todayproject/global/BaseResponseStatus.java
+++ src/main/java/com/today/todayproject/global/BaseResponseStatus.java
@@ -18,8 +18,11 @@ public enum BaseResponseStatus {
     /**
      * 2000 : Request 오류
      */
-    //sign-up
-    EXIST_EMAIL(false, 2000, "이미 존재하는 이메일입니다.");
+    EXIST_EMAIL(false, 2000, "이미 존재하는 이메일입니다."),
+    EXIST_NICKNAME(false, 2001, "이미 존재하는 닉네임입니다."),
+    NOT_FOUND_LOGIN_USER(false, 2002, "로그인한 유저가 존재하지 않습니다."),
+    NOT_FOUND_EMAIL(false, 2003, "해당 이메일이 존재하지 않습니다."),
+    SAME_NICKNAME(false, 2004, "기존 닉네임과 같은 닉네임입니다.");
 
 
     private final boolean isSuccess;
