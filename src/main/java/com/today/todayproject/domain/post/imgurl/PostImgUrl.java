ef69d6eabc3af74498685f3231aa8c6146984a02
KSH_coding <95729738+KSH-beginner@users.noreply.github.com>
2022-10-14 14:43:35 +0900

    feat : PostImgUrl(하루 이미지) 엔티티 추가

---
+++ src/main/java/com/today/todayproject/domain/post/imgurl/PostImgUrl.java
@@ -0,0 +1,33 @@
+package com.today.todayproject.domain.post.imgurl;
+
+import com.today.todayproject.domain.post.Post;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor
+public class PostImgUrl {
+
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "post_imgurl_id")
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    private Post post;
+
+    private String imgUrl;
+
+    /**
+     * 연관관계 메소드
+     */
+    public void confirmPost(Post post) {
+        this.post = post;
+        post.getPostImgUrlList().add(this);
+    }
+}
