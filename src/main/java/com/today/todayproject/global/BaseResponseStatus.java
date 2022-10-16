2ac640a61c575a147088ea2ed67b45d3ee83bc37
KSH_coding <95729738+KSH-beginner@users.noreply.github.com>
2022-10-16 14:31:54 +0900

    feat : 현재 비밀번호 불일치 예외 추가

--- src/main/java/com/today/todayproject/global/BaseResponseStatus.java
+++ src/main/java/com/today/todayproject/global/BaseResponseStatus.java
@@ -22,8 +22,8 @@ public enum BaseResponseStatus {
     EXIST_NICKNAME(false, 2001, "이미 존재하는 닉네임입니다."),
     NOT_FOUND_LOGIN_USER(false, 2002, "로그인한 유저가 존재하지 않습니다."),
     NOT_FOUND_EMAIL(false, 2003, "해당 이메일이 존재하지 않습니다."),
-    SAME_NICKNAME(false, 2004, "기존 닉네임과 같은 닉네임입니다.");
-
+    SAME_NICKNAME(false, 2004, "기존 닉네임과 같은 닉네임입니다."),
+    WRONG_CURRENT_PASSWORD(false, 2005, "현재 비밀번호가 일치하지 않습니다.");
 
     private final boolean isSuccess;
     private final int code;
