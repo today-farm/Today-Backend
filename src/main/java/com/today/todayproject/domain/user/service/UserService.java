f72c59af71f7ca4ca5d8c23d80d53f2f0a22570c
KSH_coding <95729738+KSH-beginner@users.noreply.github.com>
2022-10-16 14:34:55 +0900

    feat : 비밀번호 수정 로직 추가

--- src/main/java/com/today/todayproject/domain/user/service/UserService.java
+++ src/main/java/com/today/todayproject/domain/user/service/UserService.java
@@ -1,8 +1,11 @@
 package com.today.todayproject.domain.user.service;
 
 import com.today.todayproject.domain.user.dto.UserNicknameUpdateRequestDto;
+import com.today.todayproject.domain.user.dto.UserPasswordUpdateRequestDto;
 
 public interface UserService {
 
     void updateNickname(UserNicknameUpdateRequestDto userNicknameUpdateRequestDto) throws Exception;
+
+    void updatePassword(UserPasswordUpdateRequestDto userPasswordUpdateRequestDto) throws Exception;
 }
--- src/main/java/com/today/todayproject/domain/user/service/UserServiceImpl.java
+++ src/main/java/com/today/todayproject/domain/user/service/UserServiceImpl.java
@@ -2,11 +2,13 @@ package com.today.todayproject.domain.user.service;
 
 import com.today.todayproject.domain.user.User;
 import com.today.todayproject.domain.user.dto.UserNicknameUpdateRequestDto;
+import com.today.todayproject.domain.user.dto.UserPasswordUpdateRequestDto;
 import com.today.todayproject.domain.user.repository.UserRepository;
 import com.today.todayproject.global.BaseException;
 import com.today.todayproject.global.BaseResponseStatus;
 import com.today.todayproject.global.util.SecurityUtil;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -16,13 +18,14 @@ import org.springframework.transaction.annotation.Transactional;
 public class UserServiceImpl implements UserService {
 
     private final UserRepository userRepository;
+    private final PasswordEncoder passwordEncoder;
 
     /**
      * 닉네임 수정 로직
      */
     @Override
     public void updateNickname(UserNicknameUpdateRequestDto userNicknameUpdateRequestDto) throws Exception {
-        User loginUser = userRepository.findByEmail(SecurityUtil.getLoginUsername())
+        User loginUser = userRepository.findByEmail(SecurityUtil.getLoginUserEmail())
                 .orElseThrow(() -> new BaseException(BaseResponseStatus.NOT_FOUND_LOGIN_USER));
 
         String originalNickname = loginUser.getNickname();
@@ -36,5 +39,18 @@ public class UserServiceImpl implements UserService {
         loginUser.updateNickname(userNicknameUpdateRequestDto.getChangeNickname());
     }
 
+    /**
+     * 비밀번호 수정 로직
+     */
+    @Override
+    public void updatePassword(UserPasswordUpdateRequestDto userPasswordUpdateRequestDto) throws Exception {
+        User loginUser = userRepository.findByEmail(SecurityUtil.getLoginUserEmail())
+                .orElseThrow(() -> new BaseException(BaseResponseStatus.NOT_FOUND_LOGIN_USER));
+
+        boolean isSamePassword = loginUser
+                .matchPassword(passwordEncoder, userPasswordUpdateRequestDto.getCurrentPassword());
+        if(!isSamePassword) throw new BaseException(BaseResponseStatus.WRONG_CURRENT_PASSWORD);
+        loginUser.updatePassword(passwordEncoder, userPasswordUpdateRequestDto.getChangePassword());
+    }
 
 }
