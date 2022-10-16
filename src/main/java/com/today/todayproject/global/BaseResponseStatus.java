6243ea0e54bc06057b8c9db6e81ad67eb408e15c
KSH_coding <95729738+KSH-beginner@users.noreply.github.com>
2022-10-16 15:20:38 +0900

    feat : HttpStatus 필드 추가

--- src/main/java/com/today/todayproject/global/BaseResponseStatus.java
+++ src/main/java/com/today/todayproject/global/BaseResponseStatus.java
@@ -2,6 +2,9 @@ package com.today.todayproject.global;
 
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import org.springframework.http.HttpStatus;
+
+import static org.springframework.http.HttpStatus.*;
 
 /**
  * 에러 코드 관리
@@ -13,20 +16,21 @@ public enum BaseResponseStatus {
     /**
      * 1000 : 요청 성공
      */
-    SUCCESS(true, 1000, "요청에 성공하였습니다."),
+    SUCCESS(true, 1000, OK, "요청에 성공하였습니다."),
 
     /**
      * 2000 : Request 오류
      */
-    EXIST_EMAIL(false, 2000, "이미 존재하는 이메일입니다."),
-    EXIST_NICKNAME(false, 2001, "이미 존재하는 닉네임입니다."),
-    NOT_FOUND_LOGIN_USER(false, 2002, "로그인한 유저가 존재하지 않습니다."),
-    NOT_FOUND_EMAIL(false, 2003, "해당 이메일이 존재하지 않습니다."),
-    SAME_NICKNAME(false, 2004, "기존 닉네임과 같은 닉네임입니다."),
-    WRONG_CURRENT_PASSWORD(false, 2005, "현재 비밀번호가 일치하지 않습니다.");
+    EXIST_EMAIL(false, 2000, BAD_REQUEST, "이미 존재하는 이메일입니다."),
+    EXIST_NICKNAME(false, 2001, BAD_REQUEST, "이미 존재하는 닉네임입니다."),
+    NOT_FOUND_LOGIN_USER(false, 2002, NOT_FOUND, "로그인한 유저가 존재하지 않습니다."),
+    NOT_FOUND_EMAIL(false, 2003, NOT_FOUND,"해당 이메일이 존재하지 않습니다."),
+    SAME_NICKNAME(false, 2004, BAD_REQUEST, "기존 닉네임과 같은 닉네임입니다."),
+    WRONG_CURRENT_PASSWORD(false, 2005, BAD_REQUEST, "현재 비밀번호가 일치하지 않습니다.");
 
     private final boolean isSuccess;
     private final int code;
+    private final HttpStatus httpStatus;
     private final String message;
 
 }
