3d4cee45a257a2589d42c5552a37e1178e32c911
KSH_coding <95729738+KSH-beginner@users.noreply.github.com>
2022-10-16 14:21:48 +0900

    refactor : 메소드 이름 getLoginUsername -> getLoginUserEmail로 수정

--- src/main/java/com/today/todayproject/global/util/SecurityUtil.java
+++ src/main/java/com/today/todayproject/global/util/SecurityUtil.java
@@ -7,7 +7,7 @@ import org.springframework.security.core.userdetails.UserDetails;
  * 로그인한 Email(Username)을 받아오는 유틸 메소드 getLoginUsername()
  */
 public class SecurityUtil {
-    public static String getLoginUsername(){
+    public static String getLoginUserEmail(){
         UserDetails user = (UserDetails) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
         return user.getUsername();
     }
