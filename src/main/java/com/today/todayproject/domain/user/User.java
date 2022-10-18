6fd76e6cb866877a6423c285db6e02d41abac767
KSH_coding <95729738+KSH-beginner@users.noreply.github.com>
2022-10-18 19:53:13 +0900

    feat : updateProfileImgUrl 메소드 추가

--- src/main/java/com/today/todayproject/domain/user/User.java
+++ src/main/java/com/today/todayproject/domain/user/User.java
@@ -62,4 +62,8 @@ public class User extends BaseTimeEntity {
     public void updateNickname(String changeNickname) {
         this.nickname = changeNickname;
     }
+
+    public void updateProfileImgUrl(String changeProfileImgUrl) {
+        this.profileImgUrl = changeProfileImgUrl;
+    }
 }
