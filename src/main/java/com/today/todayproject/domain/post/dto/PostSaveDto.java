6b335458aebec2797212b500a1bfa90e54078ca1
KSH_coding <95729738+KSH-beginner@users.noreply.github.com>
2022-10-20 10:54:45 +0900

    feat : PostSaveDto 추가

---
+++ src/main/java/com/today/todayproject/domain/post/dto/PostSaveDto.java
@@ -0,0 +1,13 @@
+package com.today.todayproject.domain.post.dto;
+
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+
+@Data
+@NoArgsConstructor
+public class PostSaveDto {
+
+    private String todayFeeling;
+}
