6c5f0879529b5fa3cf5b92e39444552e4373e10b
KSH_coding <95729738+KSH-beginner@users.noreply.github.com>
2022-10-20 23:24:24 +0900

    feat : PostInfoDto 생성자 추가

--- src/main/java/com/today/todayproject/domain/post/dto/PostInfoDto.java
+++ src/main/java/com/today/todayproject/domain/post/dto/PostInfoDto.java
@@ -1,12 +1,27 @@
 package com.today.todayproject.domain.post.dto;
 
+import com.today.todayproject.domain.post.Post;
+import com.today.todayproject.domain.post.question.dto.PostQuestionInfoDto;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
 
 @Data
 @NoArgsConstructor
 public class PostInfoDto {
 
     private Long postId;
-    private List<PostQu>
+    private List<PostQuestionInfoDto> postQuestions = new ArrayList<>();
+    private String todayFeeling;
+
+    public PostInfoDto(Post post) {
+        this.postId = post.getId();
+        this.postQuestions = post.getPostQuestions().stream()
+                .map(postQuestion -> new PostQuestionInfoDto(postQuestion))
+                .collect(Collectors.toList());
+        this.todayFeeling = post.getTodayFeeling();
+    }
 }
