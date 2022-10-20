cbf2091001653095ba75519bc297b7b6274c62c0
KSH_coding <95729738+KSH-beginner@users.noreply.github.com>
2022-10-20 23:29:00 +0900

    feat : 하루 조회 API 추가

--- src/main/java/com/today/todayproject/domain/post/controller/PostController.java
+++ src/main/java/com/today/todayproject/domain/post/controller/PostController.java
@@ -1,13 +1,11 @@
 package com.today.todayproject.domain.post.controller;
 
+import com.today.todayproject.domain.post.dto.PostInfoDto;
 import com.today.todayproject.domain.post.dto.PostSaveDto;
 import com.today.todayproject.domain.post.service.PostService;
 import com.today.todayproject.global.BaseResponse;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestPart;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.util.List;
@@ -26,5 +24,11 @@ public class PostController {
             @RequestPart(required = false) List<MultipartFile> uploadVideos) throws Exception {
         postService.save(postSaveDto, uploadImgs, uploadVideos);
         return new BaseResponse<>("하루 작성에 성공하였습니다.");
+    }
+
+    @GetMapping("/{postId}")
+    public BaseResponse<PostInfoDto> getPostInfo(@PathVariable("postId") Long postId) throws Exception {
+        PostInfoDto postInfoDto = postService.getPostInfo(postId);
+        return new BaseResponse<>(postInfoDto);
     }
 }
