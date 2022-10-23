c8900134a2754d67c015388b2a63585d913de722
KSH_coding <95729738+KSH-beginner@users.noreply.github.com>
2022-10-23 11:47:35 +0900

    feat : PostUpdateDto 생성

---
+++ src/main/java/com/today/todayproject/domain/post/dto/PostUpdateDto.java
@@ -0,0 +1,15 @@
+package com.today.todayproject.domain.post.dto;
+
+import com.today.todayproject.domain.post.question.dto.PostQuestionUpdateDto;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+
+@Data
+@NoArgsConstructor
+public class PostUpdateDto {
+
+    private List<PostQuestionUpdateDto> postQuestions;
+    private String todayFeeling;
+}
