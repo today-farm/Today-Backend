8655684d01a9a6541e3739182be7cae4b058fd9e
KSH_coding <95729738+KSH-beginner@users.noreply.github.com>
2022-10-23 15:05:53 +0900

    feat : PostSaveResponseDto 추가

---
+++ src/main/java/com/today/todayproject/domain/post/dto/PostSaveResponseDto.java
@@ -0,0 +1,15 @@
+package com.today.todayproject.domain.post.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class PostSaveResponseDto {
+
+    private List<Long> postQuestionId;
+}
