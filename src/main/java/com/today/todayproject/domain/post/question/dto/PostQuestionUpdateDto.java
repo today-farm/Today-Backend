256a1877cdff529bf760ca8c532fbe8734caa749
KSH_coding <95729738+KSH-beginner@users.noreply.github.com>
2022-10-23 14:43:45 +0900

    feat : PostQuestionUpdateDto에 img,videoCount 추가

--- src/main/java/com/today/todayproject/domain/post/question/dto/PostQuestionUpdateDto.java
+++ src/main/java/com/today/todayproject/domain/post/question/dto/PostQuestionUpdateDto.java
@@ -3,12 +3,12 @@ package com.today.todayproject.domain.post.question.dto;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-import java.util.List;
-
 @Data
 @NoArgsConstructor
 public class PostQuestionUpdateDto {
 
     private Long questionId;
     private String content;
+    private int imgCount;
+    private int videoCount;
 }
