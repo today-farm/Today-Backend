157e0f6571d30af88ddb060c7f8ada523d074177
ksh-beginner <ohk9134@naver.com>
2022-10-31 23:21:26 +0900

    refactor(FriendController) : 친구 추가 API 파라미터 PathVariable로 friendId 넘기도록 리팩토링

--- src/main/java/com/today/todayproject/domain/friend/controller/FriendController.java
+++ src/main/java/com/today/todayproject/domain/friend/controller/FriendController.java
@@ -1,6 +1,5 @@
 package com.today.todayproject.domain.friend.controller;
 
-import com.today.todayproject.domain.friend.dto.FriendSaveDto;
 import com.today.todayproject.domain.friend.service.FriendService;
 import com.today.todayproject.global.BaseResponse;
 import lombok.RequiredArgsConstructor;
@@ -13,9 +12,9 @@ public class FriendController {
 
     private final FriendService friendService;
 
-    @PostMapping("/add")
-    public BaseResponse<String> add(@RequestBody FriendSaveDto friendSaveDto) throws Exception {
-        friendService.add(friendSaveDto);
+    @PostMapping("/add/{friendId}")
+    public BaseResponse<String> add(@PathVariable("friendId") Long friendId) throws Exception {
+        friendService.add(friendId);
         return new BaseResponse<>("친구 추가에 성공했습니다.");
     }
 
