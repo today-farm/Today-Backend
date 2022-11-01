63c8e10cebc599a0d6d3a6b65caed73dce1caade
ksh-beginner <ohk9134@naver.com>
2022-11-01 12:00:38 +0900

    feat(PostRepository) : 유저가 작성한 모든 게시물 조회 메소드 추가

--- src/main/java/com/today/todayproject/domain/post/repository/PostRepository.java
+++ src/main/java/com/today/todayproject/domain/post/repository/PostRepository.java
@@ -4,6 +4,11 @@ import com.today.todayproject.domain.post.Post;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+import java.util.Optional;
+
 @Repository
 public interface PostRepository extends JpaRepository<Post, Long> {
+
+    Optional<List<Post>> findAllByWriterId(Long writerId);
 }
