fb761e00ed274d1378c09a0dd4e5a55da58ef4a4
ksh-beginner <ohk9134@naver.com>
2022-11-01 17:14:37 +0900

    feat : 친구 목록 조회 API, 로직 추가

---
+++ src/main/java/com/today/todayproject/domain/friend/dto/FriendInfoDto.java
@@ -0,0 +1,16 @@
+package com.today.todayproject.domain.friend.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class FriendInfoDto {
+
+    private Long userId;
+    private String nickname;
+    private String profileImgUrl;
+    private String recentFeeling;
+}
