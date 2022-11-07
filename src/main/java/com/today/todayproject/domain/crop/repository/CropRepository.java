4a4a3808d814ab90746b77f0a30a53489244870d
ksh-beginner <ohk9134@naver.com>
2022-11-07 13:54:55 +0900

    feat(CropRepsitory) : 작물 리포지토리 추가

---
+++ src/main/java/com/today/todayproject/domain/crop/repository/CropRepository.java
@@ -0,0 +1,11 @@
+package com.today.todayproject.domain.crop.repository;
+
+import com.today.todayproject.domain.crop.Crop;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface CropRepository extends JpaRepository<Crop, Long> {
+
+    Optional<Crop> findByUserId(Long userId);
+}
