a11ee22960561a2f3ffa24208011bc56bcb0ca3f
ksh-beginner <ohk9134@naver.com>
2022-11-07 16:24:28 +0900

    feat(CropRepository) : 생성 달로 작물을 찾는 메소드 추가

--- src/main/java/com/today/todayproject/domain/crop/repository/CropRepository.java
+++ src/main/java/com/today/todayproject/domain/crop/repository/CropRepository.java
@@ -3,9 +3,12 @@ package com.today.todayproject.domain.crop.repository;
 import com.today.todayproject.domain.crop.Crop;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface CropRepository extends JpaRepository<Crop, Long> {
 
     Optional<Crop> findByUserId(Long userId);
+
+    Optional<List<Crop>> findAllByCreatedMonth();
 }
