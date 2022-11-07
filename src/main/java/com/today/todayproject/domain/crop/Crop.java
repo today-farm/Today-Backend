6684636a1849cc4748e7eb36a6e8f23031dfe3e7
ksh-beginner <ohk9134@naver.com>
2022-11-07 11:07:57 +0900

    feat(Crop) : 작물 엔티티 생성

---
+++ src/main/java/com/today/todayproject/domain/crop/Crop.java
@@ -0,0 +1,27 @@
+package com.today.todayproject.domain.crop;
+
+import com.today.todayproject.domain.user.User;
+import lombok.*;
+
+import javax.persistence.*;
+
+@Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor
+@Builder
+public class Crop {
+
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "crop_id")
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id")
+    private User user;
+
+    @Enumerated(EnumType.STRING)
+    private CropStatus status;
+
+    private int cropNumber;
+}
