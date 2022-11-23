c340354ad89224d427902daac9aaf84ecad65a1e
ksh-beginner <ohk9134@naver.com>
2022-11-24 00:20:55 +0900

    feat(PostServiceImpl) : 월별 작성한 하루 상세 조회 로직 구현

--- src/main/java/com/today/todayproject/domain/post/service/PostService.java
+++ src/main/java/com/today/todayproject/domain/post/service/PostService.java
@@ -1,25 +1,18 @@
 package com.today.todayproject.domain.post.service;
 
-import com.today.todayproject.domain.post.dto.PostInfoDto;
-import com.today.todayproject.domain.post.dto.PostSaveDto;
-import com.today.todayproject.domain.post.dto.PostSaveResponseDto;
-import com.today.todayproject.domain.post.dto.PostUpdateDto;
-import com.today.todayproject.global.BaseException;
+import com.today.todayproject.domain.post.dto.*;
 import org.springframework.web.multipart.MultipartFile;
 
-import java.time.LocalDateTime;
 import java.util.List;
 
 public interface PostService {
 
     PostSaveResponseDto save(PostSaveDto postSaveDto, List<MultipartFile> uploadImgs, List<MultipartFile> uploadVideos) throws Exception;
 
-    PostInfoDto getPostInfo(Long postId) throws Exception;
+    PostGetMonthInfoDto getUserMonthPostInfo(Long userId, int month);
 
     void update(Long postId, PostUpdateDto postUpdateDto,
                 List<MultipartFile> updateImgs, List<MultipartFile> updateVideos) throws Exception;
 
     void delete(Long postId) throws Exception;
-
-    List<String> getCreationDates(Long userId) throws Exception;
 }
