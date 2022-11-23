b6cc517692b72b75aa31a88b53faf072372b3198
ksh-beginner <ohk9134@naver.com>
2022-11-24 00:21:25 +0900

    refactor(CustomPostRepositoryImpl) : join 추가하여 쿼리문 줄이는 리팩토링

--- src/main/java/com/today/todayproject/domain/post/repository/CustomPostRepositoryImpl.java
+++ src/main/java/com/today/todayproject/domain/post/repository/CustomPostRepositoryImpl.java
@@ -3,12 +3,14 @@ package com.today.todayproject.domain.post.repository;
 import com.querydsl.jpa.impl.JPAQueryFactory;
 import com.today.todayproject.domain.post.Post;
 import com.today.todayproject.domain.post.QPost;
+import com.today.todayproject.domain.user.QUser;
 import org.springframework.stereotype.Repository;
 
 import javax.persistence.EntityManager;
 import java.util.List;
 
 import static com.today.todayproject.domain.post.QPost.post;
+import static com.today.todayproject.domain.user.QUser.user;
 
 @Repository
 public class CustomPostRepositoryImpl implements CustomPostRepository {
@@ -22,6 +24,7 @@ public class CustomPostRepositoryImpl implements CustomPostRepository {
     @Override
     public List<Post> getPostByUserIdAndMonth(Long userId, int month) {
         return query.selectFrom(post)
+                .leftJoin(post.writer, user)
                 .where(
                         post.writer.id.eq(userId),
                         post.createdDate.month().eq(month)
