9a8416acbcc61941e9a58ca6f663a3e938fcab4c
ksh-beginner <ohk9134@naver.com>
2022-11-23 16:42:50 +0900

    feat(LoginSuccessJWTProviderHandler) : 로그인 성공 시 userId 반환하도록 구현

--- src/main/java/com/today/todayproject/global/login/handler/LoginSuccessJWTProviderHandler.java
+++ src/main/java/com/today/todayproject/global/login/handler/LoginSuccessJWTProviderHandler.java
@@ -1,7 +1,12 @@
 package com.today.todayproject.global.login.handler;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.today.todayproject.domain.user.User;
 import com.today.todayproject.domain.user.repository.UserRepository;
+import com.today.todayproject.global.BaseException;
+import com.today.todayproject.global.BaseResponseStatus;
 import com.today.todayproject.global.jwt.service.JwtService;
+import com.today.todayproject.global.login.dto.LoginSuccessResponseDto;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
@@ -20,12 +25,15 @@ public class LoginSuccessJWTProviderHandler extends SimpleUrlAuthenticationSucce
 
     private final JwtService jwtService;
     private final UserRepository userRepository;
+    private final ObjectMapper objectMapper;
 
     @Value("${jwt.access.expiration}")
     private String accessTokenExpiration;
 
     @Override
-    public void onAuthenticationSuccess(HttpServletRequest request, HttpServletResponse response, Authentication authentication) throws IOException, ServletException {
+    public void onAuthenticationSuccess(HttpServletRequest request,
+                                        HttpServletResponse response, Authentication authentication) 
+            throws IOException, ServletException {
 
         String email = extractUsername(authentication); // 인증 정보에서 Username(email) 추출
         String accessToken = jwtService.createAccessToken(email); // JwtServiceImpl의 createAccessToken을 사용하여 AccessToken 발급
@@ -41,6 +49,14 @@ public class LoginSuccessJWTProviderHandler extends SimpleUrlAuthenticationSucce
                 }
         );
 
+        try {
+            LoginSuccessResponseDto loginSuccessResponseDto = new LoginSuccessResponseDto(extractUserId(authentication));
+            response.setContentType("application/json");
+            response.setCharacterEncoding("utf-8");
+            response.getWriter().write(objectMapper.writeValueAsString(loginSuccessResponseDto));
+        } catch (BaseException e) {
+            throw new RuntimeException(e);
+        }
 
         log.info( "로그인에 성공합니다. eamil: {}" ,email);
         log.info( "AccessToken 을 발급합니다. AccessToken: {}" ,accessToken);
@@ -52,5 +68,12 @@ public class LoginSuccessJWTProviderHandler extends SimpleUrlAuthenticationSucce
     private String extractUsername(Authentication authentication){
         UserDetails userDetails = (UserDetails) authentication.getPrincipal();
         return userDetails.getUsername();
+    }
+
+    private Long extractUserId(Authentication authentication) throws BaseException {
+        String userEmail = extractUsername(authentication);
+        User findUser = userRepository.findByEmail(userEmail)
+                .orElseThrow(() -> new BaseException(BaseResponseStatus.NOT_FOUND_EMAIL));
+        return findUser.getId();
     }
 }
