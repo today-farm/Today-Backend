703b7eaeeca40f6395b68baea66fab0228792d77
ksh-beginner <ohk9134@naver.com>
2022-11-23 16:42:21 +0900

    feat(LoginSuccessResponseDto) : 로그인 성공 시 반환하는 DTO  생성

---
+++ src/main/java/com/today/todayproject/global/login/dto/LoginSuccessResponseDto.java
@@ -0,0 +1,13 @@
+package com.today.todayproject.global.login.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+public class LoginSuccessResponseDto {
+
+    private Long userId;
+}
