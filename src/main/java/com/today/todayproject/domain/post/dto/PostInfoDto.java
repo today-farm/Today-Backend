d12c2ad5f66a36b7e63d5aef99dbb4943b2cb495
ksh-beginner <ohk9134@naver.com>
2022-11-23 23:58:25 +0900

    refactor(PostInfoDto) : 생성 날짜 필드 & 날짜 변환 로직 추가

--- src/main/java/com/today/todayproject/domain/post/dto/PostInfoDto.java
+++ src/main/java/com/today/todayproject/domain/post/dto/PostInfoDto.java
@@ -5,6 +5,7 @@ import com.today.todayproject.domain.post.question.dto.PostQuestionInfoDto;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Collectors;
@@ -14,14 +15,27 @@ import java.util.stream.Collectors;
 public class PostInfoDto {
 
     private Long postId;
+    private String creationDate;
     private List<PostQuestionInfoDto> postQuestions = new ArrayList<>();
     private String todayFeeling;
 
     public PostInfoDto(Post post) {
         this.postId = post.getId();
+        this.creationDate = convertLocalDateTimeFormat(post);
         this.postQuestions = post.getPostQuestions().stream()
                 .map(postQuestion -> new PostQuestionInfoDto(postQuestion))
                 .collect(Collectors.toList());
         this.todayFeeling = post.getTodayFeeling();
+    }
+
+    private String convertLocalDateTimeFormat(Post post) {
+        LocalDateTime createdDate = post.getCreatedDate();
+        String year = String.valueOf(createdDate.getYear());
+        String month = String.valueOf(createdDate.getMonthValue());
+        int dayOfMonth = createdDate.getDayOfMonth();
+        String day = "";
+        if(dayOfMonth >= 1 && dayOfMonth < 10) day = "0" + dayOfMonth;
+        if(dayOfMonth >= 10) day = String.valueOf(dayOfMonth);
+        return year + "-" + month + "-" + day;
     }
 }
