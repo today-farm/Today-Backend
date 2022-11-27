cfdf9bbcaf072368452eb68ff6229d008c34edce
ksh-beginner <ohk9134@naver.com>
2022-11-27 12:05:39 +0900

    fix(LoginFailureHandler) : 로그인 실패 시 ResponseBody 한글 깨짐 문제 fix

--- src/main/java/com/today/todayproject/global/login/handler/LoginFailureHandler.java
+++ src/main/java/com/today/todayproject/global/login/handler/LoginFailureHandler.java
@@ -15,6 +15,8 @@ public class LoginFailureHandler extends SimpleUrlAuthenticationFailureHandler {
     @Override
     public void onAuthenticationFailure(HttpServletRequest request, HttpServletResponse response, AuthenticationException exception) throws IOException, ServletException {
         response.setStatus(HttpServletResponse.SC_BAD_REQUEST);
+        response.setCharacterEncoding("UTF-8");
+        response.setContentType("text/plain;charset=UTF-8");
         response.getWriter().write("로그인 실패! 이메일이나 비밀번호를 확인해주세요.");
         log.info("로그인에 실패했습니다. 메시지 : {}", exception.getMessage());
     }
