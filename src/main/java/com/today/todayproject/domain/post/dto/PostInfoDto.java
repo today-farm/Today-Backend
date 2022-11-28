e16fb193c1165a137db6006152ced0c68ca20fd5
ksh-beginner <ohk9134@naver.com>
2022-11-29 01:42:27 +0900

    refactor : PostInfoDto 수정

--- src/main/java/com/today/todayproject/domain/post/dto/PostInfoDto.java
+++ src/main/java/com/today/todayproject/domain/post/dto/PostInfoDto.java
@@ -5,6 +5,7 @@ import com.today.todayproject.domain.post.question.dto.PostQuestionInfoDto;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Collectors;
@@ -15,6 +16,7 @@ public class PostInfoDto {
 
     private Long postId;
     private List<PostQuestionInfoDto> postQuestions = new ArrayList<>();
+    private String creationDay;
     private String todayFeeling;
 
     public PostInfoDto(Post post) {
@@ -22,6 +24,15 @@ public class PostInfoDto {
         this.postQuestions = post.getPostQuestions().stream()
                 .map(postQuestion -> new PostQuestionInfoDto(postQuestion))
                 .collect(Collectors.toList());
+        this.creationDay = convertLocalDateTimeFormatToDay(post.getCreatedDate());
         this.todayFeeling = post.getTodayFeeling();
+    }
+
+    private String convertLocalDateTimeFormatToDay(LocalDateTime createdDate) {
+        int dayOfMonth = createdDate.getDayOfMonth();
+        String day = "";
+        if(dayOfMonth >= 1 && dayOfMonth < 10) day = "0" + dayOfMonth;
+        if(dayOfMonth >= 10) day = String.valueOf(dayOfMonth);
+        return day;
     }
 }
