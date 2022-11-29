769b6b8ed03126f9db5199f094519de2217a9899
ksh-beginner <ohk9134@naver.com>
2022-11-29 21:22:46 +0900

    feat : 하루 작성 하루에 1번 이상 작성 시 예외 추가

--- src/main/java/com/today/todayproject/global/BaseResponseStatus.java
+++ src/main/java/com/today/todayproject/global/BaseResponseStatus.java
@@ -33,7 +33,8 @@ public enum BaseResponseStatus {
     NOT_FOUND_POST_QUESTION(false, 2009, NOT_FOUND, "해당 하루 게시물의 질문을 찾을 수 없습니다."),
     NOT_FOUND_FRIEND(false, 2010, NOT_FOUND, "해당 친구를 찾을 수 없습니다."),
     NOT_FOUND_USER(false, 2011, NOT_FOUND, "해당 유저를 찾을 수 없습니다."),
-    NOT_FOUND_CROP(false, 2012,NOT_FOUND, "해당 작물을 찾을 수 없습니다.");
+    NOT_FOUND_CROP(false, 2012,NOT_FOUND, "해당 작물을 찾을 수 없습니다."),
+    POST_CAN_WRITE_ONLY_ONCE_A_DAY(false, 2013, BAD_REQUEST, "하루 작성은 하루에 1번만 가능합니다.");
 
     private final boolean isSuccess;
     private final int code;
