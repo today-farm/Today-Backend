e80080cd32ded123b7ab277f78afcb948390993d
ksh-beginner <ohk9134@naver.com>
2022-11-29 23:22:11 +0900

    feat : 수확한 작물 Repository 생성

---
+++ src/main/java/com/today/todayproject/domain/growncrop/repository/GrownCropRepository.java
@@ -0,0 +1,8 @@
+package com.today.todayproject.domain.growncrop.repository;
+
+
+import com.today.todayproject.domain.growncrop.GrownCrop;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface GrownCropRepository extends JpaRepository<GrownCrop, Long> {
+}
