b27f152867aa6522e806ef72953b6509839a6be1
ksh-beginner <ohk9134@naver.com>
2022-11-30 11:15:44 +0900

    feat : 수확한 작물 정보 Dto 추가

---
+++ src/main/java/com/today/todayproject/domain/growncrop/repository/GrownCropInfoDto.java
@@ -0,0 +1,23 @@
+package com.today.todayproject.domain.growncrop.repository;
+
+import com.today.todayproject.domain.growncrop.GrownCrop;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+public class GrownCropInfoDto {
+
+    private int cropNumber;
+    private Boolean isGoldGrownCrop;
+
+    GrownCropInfoDto(GrownCrop grownCrop) {
+        this.cropNumber = grownCrop.getCropNumber();
+        if (grownCrop.getCropNumber() == -1) {
+            this.isGoldGrownCrop = true;
+        }
+        if (grownCrop.getCropNumber() != -1) {
+            this.isGoldGrownCrop = false;
+        }
+    }
+}
