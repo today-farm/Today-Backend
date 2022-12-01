51c6901e4e0fed4fdb566d3a45ecc6eebe50a407
ksh-beginner <ohk9134@naver.com>
2022-12-01 23:51:37 +0900

    refactor : PostQuestionUpdateDto 필드 수정
    수정 시 삭제할 ImgUrlId/VideoUrlId를 보내고,
    추가할 ImgCount, VideoCount 보내도록 리팩토링

--- src/main/java/com/today/todayproject/domain/post/question/dto/PostQuestionUpdateDto.java
+++ src/main/java/com/today/todayproject/domain/post/question/dto/PostQuestionUpdateDto.java
@@ -3,12 +3,16 @@ package com.today.todayproject.domain.post.question.dto;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import java.util.List;
+
 @Data
 @NoArgsConstructor
 public class PostQuestionUpdateDto {
 
     private Long questionId;
     private String content;
-    private int imgCount;
-    private int videoCount;
+    private List<Long> deleteImgUrlId;
+    private List<Long> deleteVideoUrlId;
+    private int addImgCount;
+    private int addVideoCount;
 }
