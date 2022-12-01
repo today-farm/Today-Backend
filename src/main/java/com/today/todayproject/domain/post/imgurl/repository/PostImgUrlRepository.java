38958eabd04e72110cddac5157c90899941b7508
ksh-beginner <ohk9134@naver.com>
2022-12-01 23:50:32 +0900

    feat : PostImgUrl/VideoUrlRepository 생성

---
+++ src/main/java/com/today/todayproject/domain/post/imgurl/repository/PostImgUrlRepository.java
@@ -0,0 +1,7 @@
+package com.today.todayproject.domain.post.imgurl.repository;
+
+import com.today.todayproject.domain.post.imgurl.PostImgUrl;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface PostImgUrlRepository extends JpaRepository<PostImgUrl, Long> {
+}
---
+++ src/main/java/com/today/todayproject/domain/post/video/repository/PostVideoUrlRepository.java
@@ -0,0 +1,7 @@
+package com.today.todayproject.domain.post.video.repository;
+
+import com.today.todayproject.domain.post.video.PostVideoUrl;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface PostVideoUrlRepository extends JpaRepository<PostVideoUrl, Long> {
+}
