d465e89483f7b1a5fb5cc79015854ec268109d3a
ksh-beginner <ohk9134@naver.com>
2022-12-02 01:05:42 +0900

    feat : UserId와 HarvestedMonth로 GronwCrop을 모두 조회하는 메소드 추가

--- src/main/java/com/today/todayproject/domain/growncrop/repository/GrownCropRepository.java
+++ src/main/java/com/today/todayproject/domain/growncrop/repository/GrownCropRepository.java
@@ -4,5 +4,10 @@ package com.today.todayproject.domain.growncrop.repository;
 import com.today.todayproject.domain.growncrop.GrownCrop;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+import java.util.Optional;
+
 public interface GrownCropRepository extends JpaRepository<GrownCrop, Long> {
+
+    Optional<List<GrownCrop>> findAllByUserIdAndHarvestedMonth(Long userId, int harvestedMonth);
 }
