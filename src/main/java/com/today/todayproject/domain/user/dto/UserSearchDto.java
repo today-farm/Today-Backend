1feb49a9dff7d1f43554203f55a80b57a190145d
ksh-beginner <ohk9134@naver.com>
2022-12-06 00:02:09 +0900

    refactor : UserSearchDto @AllArgsConstructor 추가

--- src/main/java/com/today/todayproject/domain/user/dto/UserSearchDto.java
+++ src/main/java/com/today/todayproject/domain/user/dto/UserSearchDto.java
@@ -1,10 +1,12 @@
 package com.today.todayproject.domain.user.dto;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 @Data
 @NoArgsConstructor
+@AllArgsConstructor
 public class UserSearchDto {
 
     private Long loginUserId;
