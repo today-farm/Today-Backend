8c19bdc0222f2708c0437b8037972eabaa87b300
ksh-beginner <ohk9134@naver.com>
2022-12-12 21:44:19 +0900

    feat(UserSignUpRequestDto) : 유저 회원가입 시 입력 값 검증 추가

--- src/main/java/com/today/todayproject/domain/user/dto/UserSignUpRequestDto.java
+++ src/main/java/com/today/todayproject/domain/user/dto/UserSignUpRequestDto.java
@@ -1,13 +1,28 @@
 package com.today.todayproject.domain.user.dto;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Pattern;
+import javax.validation.constraints.Size;
+
 @Data
 @NoArgsConstructor
+@AllArgsConstructor
 public class UserSignUpRequestDto {
 
+    @NotBlank(message = "이메일을 입력해주세요.")
+    @Pattern(regexp = "^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\\.[a-zA-Z]+$",
+            message = "이메일 형식에 맞게 입력해주세요.")
     private String email;
+
+    @NotBlank(message = "비밀번호를 입력해주세요.")
+    @Size(min = 4, max = 12, message = "비밀번호는 4자 이상 12자 이하여야합니다.")
     private String password;
+
+    @NotBlank(message = "닉네임을 입력해주세요.")
+    @Pattern(regexp = "^[a-zA-Z0-9가-힣]+$", message = "닉네임은 숫자, 한글, 영어만 가능합니다.")
     private String nickname;
 }
