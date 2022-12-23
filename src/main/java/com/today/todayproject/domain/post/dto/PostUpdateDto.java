d0b0b215973cfab7eecbcbc049721e3f8c562ca6
ksh-beginner <ohk9134@naver.com>
2022-12-23 11:18:35 +0900

    feat : 하루 수정 관련 DTO @AllArgsConstructor 추가

--- src/main/java/com/today/todayproject/domain/post/dto/PostUpdateDto.java
+++ src/main/java/com/today/todayproject/domain/post/dto/PostUpdateDto.java
@@ -1,6 +1,7 @@
 package com.today.todayproject.domain.post.dto;
 
 import com.today.todayproject.domain.post.question.dto.PostQuestionUpdateDto;
+import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
@@ -8,6 +9,7 @@ import java.util.List;
 
 @Data
 @NoArgsConstructor
+@AllArgsConstructor
 public class PostUpdateDto {
 
     private List<PostQuestionUpdateDto> postQuestions;
--- src/main/java/com/today/todayproject/domain/post/question/dto/PostQuestionDto.java
+++ src/main/java/com/today/todayproject/domain/post/question/dto/PostQuestionDto.java
@@ -1,5 +1,6 @@
 package com.today.todayproject.domain.post.question.dto;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
@@ -7,6 +8,7 @@ import java.util.List;
 
 @Data
 @NoArgsConstructor
+@AllArgsConstructor
 public class PostQuestionDto {
 
     private String question;
