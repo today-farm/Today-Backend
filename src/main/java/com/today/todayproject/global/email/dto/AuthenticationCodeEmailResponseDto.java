1374e8ede5682fe9f0928b163fe2b12c66f97513
ksh-beginner <ohk9134@naver.com>
2023-01-05 15:39:49 +0900

    feat(AuthenticationCodeEmailResponseDto) : 이메일 인증 확인 Response DTO 추가

---
+++ src/main/java/com/today/todayproject/global/email/dto/AuthenticationCodeEmailResponseDto.java
@@ -0,0 +1,13 @@
+package com.today.todayproject.global.email.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class AuthenticationCodeEmailResponseDto {
+
+    private Boolean authSuccess;
+}
