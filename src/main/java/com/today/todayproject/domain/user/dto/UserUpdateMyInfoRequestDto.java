9d6352f60b47e1e74c7fec78b678bf85346a5d45
ksh-beginner <ohk9134@naver.com>
2023-01-11 01:05:16 +0900

    feat(UserUpdateMyInfoRequestDto) : 닉네임에 @NotBlank 추가

--- src/main/java/com/today/todayproject/domain/user/dto/UserUpdateMyInfoRequestDto.java
+++ src/main/java/com/today/todayproject/domain/user/dto/UserUpdateMyInfoRequestDto.java
@@ -4,6 +4,7 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.Pattern;
 import javax.validation.constraints.Size;
 
@@ -14,6 +15,7 @@ import static com.today.todayproject.global.validation.ValidationGroups.*;
 @AllArgsConstructor
 public class UserUpdateMyInfoRequestDto {
 
+    @NotBlank(message = "변경할 닉네임을 입력해주세요.", groups = NotBlankGroup.class)
     @Pattern(regexp = "^[a-zA-Z0-9가-힣]+$", message = "수정할 닉네임은 숫자, 한글, 영어만 가능합니다.")
     @Size(min = 2, max = 8, message = "수정할 닉네임은 2자 이상 8자 이하여야합니다.")
     private String changeNickname;
