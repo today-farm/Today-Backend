9db3d932bd3bac11e77dc15f90b9703b0f195a33
ksh-beginner <ohk9134@naver.com>
2023-01-12 01:48:22 +0900

    refactor(UserUpdateMyInfoRequestDto) : 닉네임 변경하지 않으면 값이 안 들어올 수도 있으므로 @NotBlank 제거

--- src/main/java/com/today/todayproject/domain/user/dto/UserUpdateMyInfoRequestDto.java
+++ src/main/java/com/today/todayproject/domain/user/dto/UserUpdateMyInfoRequestDto.java
@@ -16,7 +16,6 @@ import static com.today.todayproject.global.validation.ValidationGroups.*;
 @AllArgsConstructor
 public class UserUpdateMyInfoRequestDto {
 
-    @NotBlank(message = "변경할 닉네임을 입력해주세요.", groups = NotBlankGroup.class)
     @Pattern(regexp = "^[a-zA-Z0-9가-힣]+$", message = "수정할 닉네임은 숫자, 한글, 영어만 가능합니다.")
     @Size(min = 2, max = 8, message = "수정할 닉네임은 2자 이상 8자 이하여야합니다.")
     private String changeNickname;
